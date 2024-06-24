fc6abc2096c73241573afc78ef0060794f41da0e
nabu1615 <javsdev@gmail.com>
2024-06-24 07:27:49 -0500

    Fix Typescript Error ( Person - User )

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,18 +1,18 @@
-import { Person, Team } from "@/types/api";
+import { User, Team } from "@/types/api";
 import { GraphQLClient, gql, request } from "graphql-request";
 
-const endpoint = process.env.HYGRAPH_ENDPOINT || '';
-const token = process.env.HYGRAPH_TOKEN || '';
+const endpoint = process.env.HYGRAPH_ENDPOINT || "";
+const token = process.env.HYGRAPH_TOKEN || "";
 
 const graphQLClient = new GraphQLClient(endpoint, {
-    headers: {
-        "Authorization": `Bearer ${token}`,
-        "content-type": "application/json"
-    }
-})
+  headers: {
+    Authorization: `Bearer ${token}`,
+    "content-type": "application/json",
+  },
+});
 
 export const getPersonInfo = async (email: string) => {
-    const personQuery = gql`
+  const personQuery = gql`
         query PersonQuery {
             person(where: {email: "${email}"}) {
                 id
@@ -44,38 +44,35 @@ export const getPersonInfo = async (email: string) => {
                 }
             }
         }
-    `
+    `;
 
-    try {
-        const data: { person: Person } = await request(endpoint, personQuery);
+  try {
+    const data: { person: User } = await request(endpoint, personQuery);
 
-        return data.person;
-
-    } catch (error) {
-        console.error(error);
-    }
-
-}
+    return data.person;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const getTeams = async () => {
-    const teamsQuery = gql`
+  const teamsQuery = gql`
     query TeamsUnlessUser {
-        teams {
-            id
-            name
-            image {
-                url
-            }
+      teams {
+        id
+        name
+        image {
+          url
         }
+      }
     }
-    `
+  `;
 
-    try {
-        const data: { teams: Team[] } = await request(endpoint, teamsQuery);
+  try {
+    const data: { teams: Team[] } = await request(endpoint, teamsQuery);
 
-        return data.teams;
-
-    } catch (error) {
-        console.error(error);
-    }
-}
+    return data.teams;
+  } catch (error) {
+    console.error(error);
+  }
+};
