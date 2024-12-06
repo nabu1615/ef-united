d75da086feeec2d362658fda2f9c302f6edbe68b
nabu1615 <javsdev@gmail.com>
2024-12-06 12:31:55 -0500

    Turn off App - reviewing the md3s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 // Globals
 import "./globals.css";
+import { Fragment } from "react";
 
 export const metadata: Metadata = {
   title: "EF United",
@@ -22,6 +23,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const mantenance = process.env.NEXT_PUBLIC_TURN_OFF_APP;
+
+  console.log(mantenance);
+
   return (
     <html lang="en">
       <head>
@@ -32,25 +37,48 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <main className="flex flex-col items-center min-h-screen">
-          <ClerkProvider
-            localization={esES}
-            appearance={{
-              elements: {
-                footer: "hidden",
-                form: "hidden",
-                dividerText: "hidden",
-                dividerRow: "hidden",
-              },
-            }}
-          >
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-              <ClerkLoading>
-                <Loader />
-              </ClerkLoading>
-            </div>
+          {mantenance === "true" ? (
+            <div className="flex flex-col items-center justify-center h-screen w-screen">
+              <div className="flex flex-col items-center md:flex-row md:justify-center">
+                <img
+                  src="/revisando-1.jpg"
+                  alt=""
+                  width={"50%"}
+                  height={"50%"}
+                />
+                <img
+                  src="/revisando-2.jpg"
+                  alt=""
+                  width={"50%"}
+                  height={"50%"}
+                />
+              </div>
 
-            <ClerkLoaded>{children}</ClerkLoaded>
-          </ClerkProvider>
+              <h1 className="text-xl font-bold text-center my-4">
+                Estamos revisando los MD3s, por favor vuelva en unos dias.
+              </h1>
+            </div>
+          ) : (
+            <ClerkProvider
+              localization={esES}
+              appearance={{
+                elements: {
+                  footer: "hidden",
+                  form: "hidden",
+                  dividerText: "hidden",
+                  dividerRow: "hidden",
+                },
+              }}
+            >
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <ClerkLoading>
+                  <Loader />
+                </ClerkLoading>
+              </div>
+
+              <ClerkLoaded>{children}</ClerkLoaded>
+            </ClerkProvider>
+          )}
         </main>
       </body>
     </html>
