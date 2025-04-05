f118d40881676442cf197778417587c68e912e26
nabu1615 <javsdev@gmail.com>
2025-04-05 02:05:31 -0500

    Disable App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ import { Fragment } from "react";
 export default function Home() {
   return (
     <Fragment>
-      <Login />
+      <h1 className="text-3xl font-bold underline">
+        Estamos revisando los MD3s
+      </h1>
+      {/* <Login /> */}
     </Fragment>
   );
 }
